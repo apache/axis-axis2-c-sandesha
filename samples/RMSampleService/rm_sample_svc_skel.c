33fc3cb81a90fd2b8a2027758816d29481343f8c
Korale Gamaralalage Nandika Chandrasiri Jayawardana <nandika@apache.org>
2007-03-07 04:39:34 +0000

    array list macros dropped

--- samples/RMSampleService/rm_sample_svc_skel.c
+++ samples/RMSampleService/rm_sample_svc_skel.c
@@ -83,9 +83,9 @@ rm_sample_svc_init(
     /* Add the implemented operation names of the service to  
      * the array list of functions 
      */
-    AXIS2_ARRAY_LIST_ADD(svc_skeleton->func_array, env, "echoString");
-    AXIS2_ARRAY_LIST_ADD(svc_skeleton->func_array, env, "ping");
-    AXIS2_ARRAY_LIST_ADD(svc_skeleton->func_array, env, "mtomSample");
+    axis2_array_list_add(svc_skeleton->func_array, env, "echoString");
+    axis2_array_list_add(svc_skeleton->func_array, env, "ping");
+    axis2_array_list_add(svc_skeleton->func_array, env, "mtomSample");
     /* Any initialization stuff of echo service should go here */
     return AXIS2_SUCCESS;
 }
@@ -152,7 +152,7 @@ rm_sample_svc_free(axis2_svc_skeleton_t *svc_skeleton,
     /* Free the function array */
     if(svc_skeleton->func_array)
     {
-        AXIS2_ARRAY_LIST_FREE(svc_skeleton->func_array, env);
+        axis2_array_list_free(svc_skeleton->func_array, env);
         svc_skeleton->func_array = NULL;
     }
     
