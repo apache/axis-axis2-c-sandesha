35e241869ae735d64722473f6f0a20553c8438ab
Damitha N.M. Kumarage <damitha@apache.org>
2007-12-06 16:03:39 +0000

    makeing default log level trace

--- samples/rm_echo_1_1/rm_echo_1_1.c
+++ samples/rm_echo_1_1/rm_echo_1_1.c
@@ -79,7 +79,7 @@ int main(int argc, char** argv)
    
     /* Set up the environment */
     env = axutil_env_create_all("rm_echo_1_1.log", 
-            AXIS2_LOG_LEVEL_CRITICAL);
+            AXIS2_LOG_LEVEL_TRACE);
 
     /* Set end point reference of echo service */
     address = "http://127.0.0.1:9090/axis2/services/RMSampleService";
