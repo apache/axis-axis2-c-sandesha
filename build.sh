1b6804578460b09eb30999e59d1d50c495ba2800
Damitha N.M. Kumarage <damitha@apache.org>
2008-02-01 04:40:03 +0000

    Updating to new Axis2/C version 1.3.0

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./autogen.sh
-./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2=${AXIS2C_HOME}/include/axis2-1.2
+./configure --prefix=${AXIS2C_HOME} --enable-static=no --with-axis2=${AXIS2C_HOME}/include/axis2-1.3.0
 make
 make install
